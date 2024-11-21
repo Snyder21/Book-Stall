e972ae849e836f67762d0dfebf574fdc71d3ca24
Snyder21 <pkcoding21@gmail.com>
2024-11-21 20:48:02 +0530

    first commit

---
+++ Frontend/src/components/Freebook.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+import axios from "axios";
+
+import Cards from "./Cards";
+function Freebook() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+
+        const data = res.data.filter((data) => data.category === "Free");
+        console.log(data);
+        setBook(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="font-semibold text-xl pb-2">Free Offered  Books</h1>
+          <p>
+            Discover a world of knowledge and inspiration with Free Offered Books,
+            a collection designed to ignite curiosity and empower learning without cost.
+            Whether you're a student, a professional, or an avid reader, these books span
+            diverse genres and subjects, catering to every interest. From timeless classics
+            to modern educational resources, free books are a gateway to new perspectives and
+            skills. Embrace the opportunity to explore, learn, and grow—all for free.
+            Dive into the pages of these treasures and uncover the joy of reading without limits.
+          </p>
+        </div>
+
+        <div>
+          <Slider {...settings}>
+            {book.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Freebook;
